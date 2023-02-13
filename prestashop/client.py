2e97d97ee34ceccd18a4bea65c3b2da15325d35b
juanrios15 <juankrios15@gmail.com>
2023-02-13 11:42:50 -0500

    first commit

---
+++ prestashop/client.py
@@ -0,0 +1,7 @@
+import requests
+
+from prestashop.exceptions import UnauthorizedError, WrongFormatInputError, ContactsLimitExceededError
+
+
+class client(object):
+    pass
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="prestashop-python",
+    version="0.1.0",
+    description="API wrapper for Prestashop written in Python",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GearPlug/prestashop-python",
+    author="Juan Carlos Rios",
+    author_email="juankrios15@gmail.com",
+    license="MIT",
+    packages=["prestashop"],
+    install_requires=[
+        "requests",
+    ],
+    zip_safe=False,
+)
